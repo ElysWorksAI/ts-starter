7a0ec1b35cbfabf33b203808600f776669b2bcda
cagrigit-hub <cagrikutayok@hotmail.com>
2025-07-25 12:11:08 +0300

    fc

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'js'],
+    testMatch: ['**/tests/**/*.test.(ts|js)'],
+    transform: {
+        '^.+\\.(ts|tsx)$': [
+            'ts-jest',
+            { tsconfig: 'tsconfig.json' }
+        ],
+    },
+  };
